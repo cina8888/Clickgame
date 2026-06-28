1e8aa93d940fd43b951705ee0e080beaab75ddd2
cina8888 <lnlgamers9@gmail.com>
2026-06-28 07:22:28 +0700

    first

---
+++ app.js
@@ -0,0 +1,51 @@
+const supabase = supabase.createClient(
+  "https://rseucqlzkmbfaiiwhjws.supabase.co",
+  "sb_publishable_pkY5M1d6GlM0qp8b6yJH5g_vfW-REtf"
+);
+
+// nhập tên
+let username = prompt("Enter your name:");
+document.getElementById("playerName").innerText = username;
+
+let score = 0;
+
+// click tăng điểm
+document.getElementById("clickBtn").addEventListener("click", async () => {
+  score++;
+  document.getElementById("score").innerText = score;
+
+  // gửi lên database (simple version)
+  await supabase
+    .from("scores")
+    .upsert({ username: username, score: score });
+
+  loadLeaderboard();
+});
+
+// load leaderboard
+async function loadLeaderboard() {
+  let { data } = await supabase
+    .from("scores")
+    .select("*")
+    .order("score", { ascending: false });
+
+  document.getElementById("leaderboard").innerHTML =
+    data.map((p, i) =>
+      `${i + 1}. ${p.username} - ${p.score}`
+    ).join("<br>");
+}
+
+// realtime update
+supabase
+  .channel("scores")
+  .on("postgres_changes", {
+    event: "*",
+    schema: "public",
+    table: "scores"
+  }, () => {
+    loadLeaderboard();
+  })
+  .subscribe();
+
+// initial load
+loadLeaderboard();
